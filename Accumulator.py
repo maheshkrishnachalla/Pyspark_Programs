1ccad101f6af812b69ee22adf08f9a24ddf167bc
maheshkrishnachalla <maheshkrishnachalla@gmail.com>
2022-03-18 12:33:29 +0530

    updated Accumulator.py

--- Accumulator.py
+++ Accumulator.py
@@ -4,11 +4,11 @@ spark = SparkSession.builder.master("local").appName("Accumulator").enableHiveSu
 
 load_input = spark.sparkContext.textFile("D:\\BIGDATA\Spark\\samplefile.txt")
 
-my_accum = spark.sparkContext.accumulator(0)
+accumulator = spark.sparkContext.accumulator(0)
 
-result  = load_input.collect()
+result = load_input.collect()
 for i in result:
     if i == "":
-        my_accum.add(1)
+        accumulator.add(1)
 
-print(my_accum)
+print(accumulator)
